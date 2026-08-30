7ccb7222b368104efebe33515069fb0dc056f8cf
Joshua Marie <joshstat126@gmail.com>
2026-08-30 17:46:03 +0800

    Restore old setups

--- setup-modules.r
+++ setup-modules.r
@@ -1,3 +1,34 @@
+if (!file.exists("_quarto.yml")) {
+    stop("run this from the project root, next to _quarto.yml")
+}
+
+chapters = list.dirs(".", recursive = FALSE, full.names = FALSE)
+chapters = chapters[grepl("^Chapter [0-9]+$", chapters)]
+
+extract_r_chunks = function(file) {
+    lines = readLines(file, warn = FALSE)
+    chunk_start = grep("^```\\{r", lines)
+    chunk_end = grep("^```\\s*$", lines)
+    
+    chunks = character(0)
+    for (start in chunk_start) {
+        end = chunk_end[chunk_end > start][1]
+        if (!is.na(end)) {
+            chunks = c(chunks, lines[(start + 1):(end - 1)])
+        }
+    }
+    chunks
+}
+
+needs_module = function(chapter) {
+    files = list.files(chapter, pattern = "\\.qmd$", full.names = TRUE)
+    any(vapply(files, function(f) any(grepl("\\./module", extract_r_chunks(f))), logical(1)))
+}
+
+chapters = chapters[vapply(chapters, needs_module, logical(1))]
+
+target = normalizePath("module")
+
 link_module = function(chapter) {
     link = file.path(chapter, "module")
     
@@ -6,15 +37,13 @@ link_module = function(chapter) {
         return(invisible())
     }
     
-    ok = if (.Platform$OS.type == "windows") {
-        shell(sprintf('mklink /J "%s" "%s"', link, target)) == 0
+    if (.Platform$OS.type == "windows") {
+        shell(sprintf('mklink /J "%s" "%s"', link, target))
     } else {
         file.symlink(target, link)
     }
     
-    if (!ok) {
-        stop("failed to link ", link, " -> ", target)
-    }
-    
     message("linked ", link, " -> ", target)
 }
+
+invisible(lapply(chapters, link_module))
